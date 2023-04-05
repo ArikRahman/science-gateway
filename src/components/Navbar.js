865413ae4794da50750ebdfe683dd990d372b05a
kristinetlam <kristinetlam21@gmail.com>
2023-04-05 17:00:50 -0500

    Update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,38 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {ReactComponent as TAMULogo} from './TAM-PrimaryMarkB-white.svg';
 import './NavbarStyles.css';
 
 
-function Navbar() { // function name is always file name
-    return (
-        <>
-            <nav>
-                <a href="index.html">
-                    <TAMULogo />
-                </a>
+class Navbar extends Component {
+    state = { clicked: false };
+    handleClick = () => {
+        this.setState({clicked: !this.state.clicked})
+    }
+    render() {
+        return (
+            <>
+                <nav>
+                    <a href="index.html">
+                        <TAMULogo />
+                    </a>
 
-                <div>
-                    <ul id="navbar"> {/* create an id to fix the list in CSS */}
-                        <li><a className="active"
-                        href="index.html">Data</a></li>
-                        <li><a href="index.html">
-                            API
-                            </a>
-                        </li>
-                        <li><a href="index.html">VPN</a></li>
-                        <li><a href="index.html">Contact</a></li>
-                        <li><a href="index.html">About Us</a></li>
-                    </ul>
+                    <div>
+                        <ul id="navbar" className={this.state.clicked ? "#navbar active" : "#navbar"}> {/* create an id to fix the list in CSS */}
+                            <li><a className="active"
+                            href="index.html">Data</a></li>
+                            <li><a href="index.html">
+                                API
+                                </a>
+                            </li>
+                            <li><a href="index.html">VPN</a></li>
+                            <li><a href="index.html">Contact</a></li>
+                            <li><a href="index.html">About Us</a></li>
+                        </ul>
+                    </div>
 
-                </div>
-
-                <div id="mobile"> {/* create responsive navbar for mobile */}
-                    <i className="fas fa-bars"></i> {/* hamburger icon navbar (3 lines stacked)*/}
-                    <i className="fas fa-times"></i> {/* close icon */}
-                </div>
-            </nav>
-        </>
+                    <div id="mobile" onClick = {this.handleClick}> {/* create responsive navbar for mobile */}
+                        <i id="bar"
+                        className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}
+                        ></i>
+                    </div>
+                </nav>
+            </>
     );
-};
+}
+}
 
 export default Navbar;
